24e8a88aa32d7834f4b280bf8fd47508e9436561
Ansh <lightnarcissus@gmail.com>
2015-01-24 08:16:54 -0500

    Chair and Guy Animations
    Plus created Images folder

---
+++ Assets/GuyRecline.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuyRecline : KeyTriggeredBehavior {
+
+	// Use this for initialization
+	public override void Start () {
+        gameObject.GetComponent<Animator>().enabled = false; 
+	}
+	
+	// Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+	}
+
+    public override void PlayAction()
+    {
+        gameObject.GetComponent<Animator>().enabled = true; //triggers GuyRecline movement
+    }
+}
---
+++ Assets/MoveChair.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveChair : KeyTriggeredBehavior
+{
+
+    // Use this for initialization
+    public override void Start()
+    {
+        gameObject.GetComponent<Animator>().enabled = false;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void PlayAction()
+    {
+        gameObject.GetComponent<Animator>().enabled = true; //triggers ChairRecline movement
+    }
+}
