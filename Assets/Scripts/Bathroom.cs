1063a127f3342b4484ed900f5cac7acf804eb94a
Owen Bell <owenalexbell@gmail.com>
2015-01-24 23:33:54 -0500

    You can now go the the bathroom

---
+++ Assets/Scripts/Bathroom.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bathroom : KeyTriggeredBehavior {
+
+	private bool __bladderFull = true;
+
+	public override void PlayAction() {
+		if (__bladderFull) {
+			_animator.SetTrigger("go");
+			__bladderFull = false;
+		}
+	}
+
+	public override void PlayUpAction() {
+
+	}
+
+	public void SetBladderFull() {
+		// Debug.Log("I have to go");
+		__bladderFull = true;
+	}
+
+	public void PlayFlush() {
+		_audio.Play();
+	}
+
+}
