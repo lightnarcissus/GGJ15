12e428f70fe7f8d37a3c6a6058d6fd2a2fa64a9e
Owen Bell <owenalexbell@gmail.com>
2015-01-25 15:15:14 -0500

    Background bobs, watch and phone trigger sounds. Build settings are now set for final.

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public enum CurrentDisplay {
 	plane,
 	watch,
-	phone
+	phone,
+	title,
+	captain,
 }
 
 public class CanvasController : MonoBehaviour {
@@ -13,6 +15,7 @@ public class CanvasController : MonoBehaviour {
 	public GameObject _instructions;
 	public GameObject _watch;
 	public GameObject _smartphone;
+	public GameObject _title;
 
 	private bool __checkedPhone = false;
 	private bool __checkedWatch = false;
@@ -21,45 +24,53 @@ public class CanvasController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		// _display = CurrentDisplay.plane;	
+		_title.active = true;
+		_display = CurrentDisplay.title;	
 		// _introPanel.active = true;
-		DisplayCaptain();
+		// DisplayCaptain();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.Space)) {
-			_introPanel.active = false;
-			ReturnToPlane(CurrentDisplay.plane);
+			if (_display == CurrentDisplay.title) {
+				_title.active = false;
+				DisplayCaptain();
+			} else if (_display == CurrentDisplay.captain) {
+				_introPanel.active = false;
+				ReturnToPlane(CurrentDisplay.captain);
+			}
 		}	
 	}
 
 	public void DisplayCaptain() {
 		_introPanel.active = true;
-		_display = CurrentDisplay.plane;
+		_display = CurrentDisplay.captain;
 		GetComponent<AudioSource>().Play();
 	}
 
 	public void DisplayPhone() {
 		if (!__checkedPhone) {
-			_smartphone.active = true;
 			__checkedPhone = true;
 		}
+		_smartphone.active = true;
 		_introPanel.active = false;
 		_instructions.active = false;
 		_watch.active = false;
 		_display = CurrentDisplay.phone;
+		GetComponent<AudioSource>().Stop();
 	}
 
 	public void DisplayWatch() {
 		if (!__checkedWatch) {
 			__checkedWatch = true;
-			_watch.active = true;
 		}
+		_watch.active = true;
 		_introPanel.active = false;
 		_instructions.active = false;
 		_smartphone.active = false;
 		_display = CurrentDisplay.watch;
+		GetComponent<AudioSource>().Stop();
 	}
 
 	public void ReturnToPlane(CurrentDisplay returnFrom) {
@@ -70,6 +81,7 @@ public class CanvasController : MonoBehaviour {
 			_smartphone.active = false;
 			_watch.active = false;
 			_display = CurrentDisplay.plane;
+			GetComponent<AudioSource>().Stop();
 		}
 	}
 }
